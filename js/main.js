50d680e382ef411c39878fe7ff8daa4efd5a24a2
TamirBelisha <98407186+TamirBelisha@users.noreply.github.com>
2022-02-07 20:11:23 +0200

    Last commit
    added new links and images

--- js/main.js
+++ js/main.js
@@ -1,1 +1,103 @@
+'use strict';
 console.log('Starting up');
+
+$(initPage)
+
+function initPage() {
+    renderProjs();
+}
+
+function renderProjs() {
+    var projs = getProjs();
+    var $elPortfolioSection = $('#portfolio')
+    var strHtml = `
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Portfolio</h2>
+                <h3 class="section-subheading text-muted">Some projects made by me</h3>
+            </div>
+        </div>
+        <div class="row">
+        `
+
+    var arrHtml = projs.map((proj) => {
+        return `
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal">
+                        <div class="portfolio-hover" onclick="renderModal('${proj.id}')">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img class="img-fluid" src="img/portfolio/${proj.id} thumbnail.png" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>${proj.name}</h4>
+                        <p class="text-muted">${proj.title}</p>
+                    </div>
+                </div>
+            `
+    })
+    strHtml += arrHtml.join('');
+    strHtml += `</div></div>`
+
+    $elPortfolioSection.html(strHtml);
+
+}
+
+function renderModal(projId) {
+    console.log('projId', projId);
+    console.log('got to render modal');
+    var proj = getProjById(projId)
+    var strHtml = `
+    <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="close-modal" data-dismiss="modal">
+                        <div class="lr">
+                            <div class="rl"></div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <div class="modal-body">
+                                    <!-- Project Details Go Here -->
+                                    <h2>${proj.name}</h2>
+                                    <p class="item-intro text-muted"></p>
+                                    <img class="img-fluid d-block mx-auto" src="img/portfolio/${proj.id}.png" alt="">
+                                    <p>${proj.desc}</p>
+                                    <ul class="list-inline">
+                                        <li>Date: ${proj.publishedAt}</li>
+                                        <li>Client: Coding Academy</li>
+                                        <li>Category: ${proj.title}</li>
+                                    </ul>
+                                    <a href="${proj.url}" target="_blank">
+                                    <button class="btn btn-primary" type="button">
+                                    Open Project</button>
+                                    </a>
+                                    <button class="btn btn-primary" data-dismiss="modal" type="button">
+                        <i class="fa fa-times"></i>
+                        Close Project</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+    $('.modal').html(strHtml)
+}
+
+function onSubmit() {
+    var $elSubjectVal = $('#form-Subject').val()
+    var $elTextVal = $('#form-text').val()
+    var resSubject = $elSubjectVal.split(' ').join('%20')
+    var resText = $elTextVal.split(' ').join('%20')
+
+    window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=TamirBelisha@gmail.com&su=${resSubject}&body=${resText}`, '_blank')
+    $('#form-name').val('')
+    $('#form-email').val('')
+    $('#form-Subject').val('')
+    $('#form-text').val('')
+}
---
+++ js/services/gallery-service.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gProjs = createProjs();
+
+function getProjs() {
+    return gProjs;
+}
+
+
+function createProjs() {
+    return [
+        createProj('Day 09 tasks', 'Two basic projects', 'https://tamirbelisha.github.io/CaJan22-Day09/', 'January 2022'),
+        createProj('Mine sweeper', 'Board game', 'https://tamirbelisha.github.io/Mine-Sweeper/', 'January 2022'),
+        createProj('Pacman', 'Board game', 'https://tamirbelisha.github.io/CaJan22-Pacman/', 'January 2022'),
+        createProj('Ball board', 'Board game', 'https://tamirbelisha.github.io/ball-board/', 'January 2022'),
+        createProj('Book shop', 'Online book shop managment', 'https://tamirbelisha.github.io/book-shop/', 'January 2022'),
+    ]
+}
+
+function createProj(projName, title, url, publishedAt) {
+
+    return {
+        id: projName.toLowerCase(),
+        name: projName,
+        title,
+        desc: makeLorem(12),
+        url,
+        publishedAt,
+        labels: null
+    }
+}
+
+function getProjById(projId) {
+    return gProjs.find(proj => proj.id === projId)
+}
