3211b04e5947a304a1d4f15669302793292cb6f8
Tamir Belisha <Tamirbelisha@gmail.com>
2022-02-08 14:24:29 +0200

    Last commit - not rendering contact form

--- js/main.js
+++ js/main.js
@@ -5,28 +5,16 @@ $(initPage);
 
 function initPage() {
     renderProjs();
-    renderContactForm();
 }
 
 function renderProjs() {
     var projs = getProjs();
-    var $elPortfolioSection = $('#portfolio')
-    var strHtml = `
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <h2 class="section-heading">Portfolio</h2>
-                <h3 class="section-subheading text-muted">Some projects made by me</h3>
-            </div>
-        </div>
-        <div class="row">
-        `
-
+    var $elPortfolioDiv = $('#portfolio')
     var arrHtml = projs.map((proj) => {
         return `
                 <div class="col-md-4 col-sm-6 portfolio-item">
                     <a class="portfolio-link" data-toggle="modal" href="#portfolioModal">
-                        <div class="portfolio-hover" onclick="renderModal('${proj.id}')">
+                        <div class="portfolio-hover">
                             <div class="portfolio-hover-content">
                                 <i class="fa fa-plus fa-3x"></i>
                             </div>
@@ -40,14 +28,18 @@ function renderProjs() {
                 </div>
             `
     })
-    strHtml += arrHtml.join('');
-    strHtml += `</div></div>`
+    var strHtml = arrHtml.join('');
 
-    $elPortfolioSection.html(strHtml);
-
+    $elPortfolioDiv.html(strHtml);
+    $('.portfolio-item').each(function(idx, el) {
+        console.log(el);
+        $(el).click({ projId: projs[idx].id }, renderModal)
+    })
 }
 
-function renderModal(projId) {
+function renderModal(ev) {
+    console.log(ev);
+    var { projId } = ev.data
     console.log('projId', projId);
     console.log('got to render modal');
     var proj = getProjById(projId)
@@ -101,63 +93,4 @@ function onSubmit() {
     $('#form-email').val('')
     $('#form-Subject').val('')
     $('#form-text').val('')
-}
-
-
-function renderContactForm() {
-    $('.offcanvas-aside').html(`
-    <!-- Section: Contact v.1 -->
-        <section class="my-5">
-
-            <!-- Section heading -->
-            <h2 class="h1-responsive font-weight-bold text-center my-5">Contact</h2>
-            <!-- Section description -->
-            <p class="text-center w-responsive mx-auto pb-1 text-light">+972-54-263-1200</p>
-            <p class="text-center w-responsive mx-auto pb-1 text-light">TamirBelisha@gmail.com</p>
-
-            <!-- Grid row -->
-            <div class="row">
-
-                <!-- Grid column -->
-                <div class="col-12 mb-lg-0 mb-4">
-
-                    <!-- Form with header -->
-                    <div class="card">
-                        <div class="card-body">
-                            <!-- Header -->
-                            <div class="form-header blue accent-1">
-                                <h3 class="mt-2"><i class="fa fa-envelope"></i> Contact me:</h3>
-                            </div>
-                            <p class="text-muted">I'll write back A S A P</p>
-                            <!-- Body -->
-                            <div class="md-form">
-                                <i class="fa fa-user prefix grey-text"></i>
-                                <label for="form-name">Your name</label>
-                                <input type="text" id="form-name" class="form-control">
-                            </div>
-                            <div class="md-form">
-                                <i class="fa fa-envelope prefix grey-text"></i>
-                                <label for="form-email">Your email</label>
-                                <input type="text" id="form-email" class="form-control">
-                            </div>
-                            <div class="md-form">
-                                <i class="fa fa-tag prefix grey-text"></i>
-                                <label for="form-Subject">Subject</label>
-                                <input type="text" id="form-Subject" class="form-control">
-                            </div>
-                            <div class="md-form">
-                                <i class="fa fa-pencil prefix grey-text"></i>
-                                <label for="form-text">Your message</label>
-                                <textarea id="form-text" class="form-control md-textarea" rows="3"></textarea>
-                            </div>
-                            <div class="text-center">
-                                <button class="btn btn-light-blue" onclick="onSubmit()">Submit</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </section>
-    `)
 }
